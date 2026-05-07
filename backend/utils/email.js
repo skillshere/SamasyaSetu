6828763e18456161d94e55a1707e024369ac480c
skillshere <rk4736048@gmail.com>
2026-05-07 22:35:27 +0530

    Fixed nodemailer SMTP config

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,10 +1,9 @@
-const nodemailer = require("nodemailer");
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
+const nodemailer = require('nodemailer');
 
-// transporter setup
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
